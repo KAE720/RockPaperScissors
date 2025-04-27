e77ba5c9e27412a6cac1e350aa7bc25c87bc96a4
Kerem <keremerol72gmail.comgit config --global user.emailgit config --global user.email keremerol72gmail.com>
2025-04-27 20:32:04 +0100

    ignore other folder

--- rps.js
+++ rps.js
@@ -7,129 +7,99 @@ let computerScore = 0;
 
 
 
-
-
-
-
-
-
-
-
-while (true){
-
-    function getComputerChoice() {
+    function computerChoice() {
 
         if ((Math.floor(Math.random() * 3) + 1) == 1) {
             console.log(rock)
             return rock;
         }
-    
+
         else if ((Math.floor(Math.random() * 3) + 1) == 2) {
             console.log(paper)
             return paper;
         }
-    
+
         else {
             console.log(scissors)
             return scissors
         }
     }
-    
-    let computerChoice = getComputerChoice();
-    
 
-    let userinput = prompt("What is your choice?")
+    let getComputerChoice = computerChoice();
 
 
-function getHumanChoice(userinput) {
 
-    if (userinput == "rock") {
-        console.log(userinput)
-        return rock;
-    }
 
-    else if (userinput == "paper") {
-        console.log(userinput)
-        return paper
-    }
 
-    else {
-        console.log(userinput)
-        return scissors
-    }
+    function playRound(humanChoice, getComputerChoice) {
 
-}
+        if (humanChoice == "rock" && getComputerChoice == "paper") {
 
-let humanChoice = getHumanChoice(userinput);
-
-   
-
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice == "rock" && computerChoice == "paper") {
-            
             console.log("YOU LOSE")
             computerScore++;
         }
 
-        else if (humanChoice == "rock" && computerChoice == "scissors") {
+        else if (humanChoice == "rock" && getComputerChoice == "scissors") {
             humanScore++;
             console.log("YOU WIN")
         }
 
-        else if (humanChoice == "rock" && computerChoice == "rock") {
+        else if (humanChoice == "rock" && getComputerChoice == "rock") {
             console.log("draw")
             alert("draw")
         }
 
-        else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        else if (humanChoice == "scissors" && getComputerChoice == "scissors") {
             console.log("draw")
         }
 
-        else if (humanChoice == "scissors" && computerChoice == "paper") {
+        else if (humanChoice == "scissors" && getComputerChoice == "paper") {
             console.log("YOU WIN")
             humanScore++;
         }
 
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
+        else if (humanChoice == "scissors" && getComputerChoice == "rock") {
             console.log("YOU LOSE")
             computerScore++;
-        
+
         }
 
-        else if (humanChoice == "paper" && computerChoice == "paper") {
+        else if (humanChoice == "paper" && getComputerChoice == "paper") {
             console.log("draw")
         }
 
-        else if (humanChoice == "paper" && computerChoice == "scissors") {
+        else if (humanChoice == "paper" && getComputerChoice == "scissors") {
             console.log("YOU LOSE")
             computerScore++;
-            
+
         }
 
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
+        else if (humanChoice == "scissors" && getComputerChoice == "rock") {
             console.log("YOU WIN")
             humanScore++;
         }
 
-        
+
         console.log(humanScore);
     }
 
 
-    
-    playRound(humanChoice, computerChoice)
 
-    if (humanScore == 5){
-        break;
-    }
+    let button1 = document.querySelector(".rock")
+    let button2 = document.querySelector(".paper")
+    let button3 = document.querySelector(".scissors")
 
-    if (computerScore == 5){
-        break;
-    }
+    button1.addEventListener("click", function () {
+        playRound("rock", getComputerChoice)
+    })
 
 
+    button2.addEventListener("click", function () {
+        playRound("paper", getComputerChoice)
+    })
 
-}
+    button3.addEventListener("click", function () {
+        playRound("scissors", getComputerChoice)
+    })
 
 
